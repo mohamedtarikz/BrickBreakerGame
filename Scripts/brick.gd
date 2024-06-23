68848930ddc4ec442685a2e6b1e7883210b77d23
mohamedtarikz <mohamedtarik06@gmail.com>
2024-06-23 13:45:15 +0300

    Updated project with new assets

--- Scripts/brick.gd
+++ Scripts/brick.gd
@@ -5,19 +5,26 @@ class_name Brick
 @onready var sprite = $Sprite2D
 
 @export var level = 0
+@export var color = 0
 
 var degrs = [
-	preload("res://Assets/Sprites/Gbrick.png"),
-	preload("res://Assets/Sprites/Bbrick.png"),
-	preload("res://Assets/Sprites/Rbrick.png")
+	[preload("res://Assets/Sprites/Gbrick.png"), preload("res://Assets/Sprites/GBrickbroken.png")],
+	[preload("res://Assets/Sprites/BBrick.png"), preload("res://Assets/Sprites/BBrickbroken.png")],
+	[preload("res://Assets/Sprites/RBrick.png"), preload("res://Assets/Sprites/RBrickbrocken.png")]
 ]
 
 signal destroyed
 
 func set_texture(newlevel):
 	level = newlevel
-	if level >= 1:
-		sprite.texture = degrs[level - 1]
+	if color >= 0:
+		if level == 2:
+			sprite.texture = degrs[color][0]
+		elif level == 1:
+			sprite.texture = degrs[color][1]
+		else:
+			color -= 1
+			set_texture(2)
 	else:
 		destroyed.emit()
 		queue_free()
